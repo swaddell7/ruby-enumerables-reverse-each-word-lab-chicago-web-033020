2c9eda9c2deab403b3fad3dbb6d416aafa57e0c2
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-20 05:23:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   array = string.split
   new_array = array.collect{|word| word.reverse}
+  new_array.join(' ')
 end  
 
 #array.reverse will put the array in reverse order but not the letters in each word 
