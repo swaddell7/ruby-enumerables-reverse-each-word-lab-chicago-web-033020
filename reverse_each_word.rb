692b858bc52f5daa2ec5cbd29c17c661fe8cc39e
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-20 05:11:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,8 @@ def reverse_each_word(string)
   array = string.split
   index = 0
   while index < array.length do
-    array.each{|array| array[index].reverse
+    array.each{|array| array[index].reverse}
+    index += 1
+  end
+  
 end  
